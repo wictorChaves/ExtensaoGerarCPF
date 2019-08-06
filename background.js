112a2a93ed18e2d3866c12447341774281c4c136
Wictor Chaves <wictor@hotmail.com.br>
2019-08-06 00:01:31 -0300

    Código mais legível

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
-    SetTransferArea(gerar(false));
+    SetTransferArea(gerar());
 });
 
 function SetTransferArea(str) {
@@ -10,32 +10,34 @@ function SetTransferArea(str) {
     document.execCommand("copy", false, null);
 }
 
-function gerar(comPontos) {
-    var n = 9;
-    var n1 = randomiza(n);
-    var n2 = randomiza(n);
-    var n3 = randomiza(n);
-    var n4 = randomiza(n);
-    var n5 = randomiza(n);
-    var n6 = randomiza(n);
-    var n7 = randomiza(n);
-    var n8 = randomiza(n);
-    var n9 = randomiza(n);
-    var d1 = n9 * 2 + n8 * 3 + n7 * 4 + n6 * 5 + n5 * 6 + n4 * 7 + n3 * 8 + n2 * 9 + n1 * 10;
-    d1 = 11 - (mod(d1, 11));
-    if (d1 >= 10) d1 = 0;
-    var d2 = d1 * 2 + n9 * 3 + n8 * 4 + n7 * 5 + n6 * 6 + n5 * 7 + n4 * 8 + n3 * 9 + n2 * 10 + n1 * 11;
-    d2 = 11 - (mod(d2, 11));
-    if (d2 >= 10) d2 = 0;
-    retorno = '';
-    if (comPontos) retorno = '' + n1 + n2 + n3 + '.' + n4 + n5 + n6 + '.' + n7 + n8 + n9 + '-' + d1 + d2;
-    else retorno = '' + n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + d1 + d2;
-    return retorno;
+function gerar() {
+    var digitosInicial = randomArray();
+    var penultimoDigito = ajuste(somar(digitosInicial));
+    var ultimoDigito = ajuste(somar(digitosInicial, (penultimoDigito * 2), 3));
+    return digitosInicial.join('') + penultimoDigito + ultimoDigito;
+}
+
+function ajuste(soma) {
+    soma = 11 - (mod(soma, 11));
+    return (soma >= 10) ? 0 : soma;
+}
+
+function somar(array, soma = 0, count = 2) {
+    for (i = 8; i >= 0; i--) {
+        soma = (array[i] * count) + soma;
+        count++;
+    }
+    return soma;
+}
+
+function randomArray(array = []) {
+    for (i = 0; i < 9; i++)
+        array.push(randomiza(9))
+    return array;
 }
 
 function randomiza(n) {
-    var ranNum = Math.round(Math.random() * n);
-    return ranNum;
+    return Math.round(Math.random() * n);
 }
 
 function mod(dividendo, divisor) {
